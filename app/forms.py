4e4754656a71055964f79a1ca7e36c4a511bbcf4
MichaelLaguatan <michael.laguatan03@gmail.com>
2026-03-20 10:56:15 -0600

    HERE YOU GO

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
-import sqlalchemy as sa
-from app import db
-from app.models import User
+from wtforms import StringField, RadioField, SubmitField, FormField, FieldList
+from wtforms.validators import DataRequired
 
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username (First and Last)', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class QuestionForm(FlaskForm):
+    question_text = StringField()
+    options = RadioField()
+
+class QuizForm(FlaskForm):
+    questions = FieldList(FormField(QuestionForm), label='')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,28 +1,25 @@
 from app.main import bp
 from flask import render_template, flash, redirect, url_for
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, QuestionForm, QuizForm
 from app.models import User, Questions
 from app import db
 import csv
 import sqlalchemy as sa
-
-
-@bp.route('/read_csv')
-def read_csv():
-    with open('sample_questions.csv', mode = 'r') as file:
-        csvFile = csv.reader(file)
-        for lines in csvFile:
-            question = Questions(lines[0], lines[1], lines[2], lines[3], lines[4], lines[5])
-            db.session.add(question)
-    db.session.commit()
-
-
+import logging
 
 @bp.route('/')
 @bp.route('/index')
 def index():
+    form = QuizForm()
     db_questions = db.session.scalars(sa.select(Questions)).all()
-    return render_template('index.html', title='Questions', db_questions=db_questions)
+    quiz_data = [{"question": question, "choices": [("a", question.option1), ("b", question.option2), ("c", question.option3), ("d", question.option4)]} for question in db_questions]
+    for q in quiz_data:
+      form.questions.append_entry()
+    for i, q in enumerate(quiz_data):
+      question_form = form.questions[i].form
+      question_form.question_text.label = q["question"]
+      question_form.options.choices = q["choices"]
+    return render_template('index.html', title='Questions', db_questions=db_questions, form=form)
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -36,4 +33,17 @@ def register():
     return render_template('register.html', title='Enter Name', form=form)
 
 
-
+@bp.route('/read_csv')
+def read_csv():
+    questions = []
+    db.session.query(Questions).delete()
+    with open('sample_questions.csv', mode = 'r', encoding='utf-8-sig') as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            question = Questions(question=lines[0], option1=lines[1], option2=lines[2], option3=lines[3], option4=lines[4], correct_option=lines[5])
+            db.session.add(question)
+            questions.append((lines[0], lines[1], lines[2], lines[3], lines[4], lines[5]))
+    db.session.commit()
+    for question in questions:
+        print(question)
+    return questions
